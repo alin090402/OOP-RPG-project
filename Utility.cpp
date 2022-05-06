78c4d03aba8b8e5fd1c29b123ba118a06ff220d9
alin090402 <alin090402@yahoo.ro>
2022-05-06 19:10:25 +0300

    minor modifications

--- Utility.cpp
+++ Utility.cpp
@@ -38,10 +38,8 @@ std::vector<std::string> Utility::ReadFile(const std::string &fileName) {
     std::vector<std::string> lines;
     std::ifstream file(fileName);
     std::string line;
-    while (std::getline(file, line))
-    {
+    while(std::getline(file, line))
         lines.push_back(line);
-    }
     return lines;
 }
 
